b80d85e5faaa57a66146e5cbd2cbe0c192cd3a4a
Jeremy Magland <jeremy.magland@gmail.com>
2017-02-16 11:29:51 -0500

    add

---
+++ test_comparison/001_generate_synthetic_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+opts="--firing_rate_min=0.5 --firing_rate_max=3 --M=8 --duration=1200"
+
+./generate_synth_examples.node.js $opts --dsname=example1 --K=3 --noise_level=2
+./generate_synth_examples.node.js $opts --dsname=example2 --K=8 --noise_level=2
+./generate_synth_examples.node.js $opts --dsname=example3 --K=16 --noise_level=2
