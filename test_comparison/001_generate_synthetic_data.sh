d43a1055e35effb3e617a8ad40bbb55577084b34
Jeremy Magland <jeremy.magland@gmail.com>
2017-02-16 11:35:12 -0500

    adjust

--- test_comparison/001_generate_synthetic_data.sh
+++ test_comparison/001_generate_synthetic_data.sh
@@ -2,6 +2,6 @@
 
 opts="--firing_rate_min=0.5 --firing_rate_max=3 --M=8 --duration=1200"
 
-./generate_synth_examples.node.js $opts --dsname=example1 --K=3 --noise_level=2
-./generate_synth_examples.node.js $opts --dsname=example2 --K=8 --noise_level=2
-./generate_synth_examples.node.js $opts --dsname=example3 --K=16 --noise_level=2
+./generate_synth_examples.node.js $opts --dsname=example1 --K=3 --noise_level=1
+./generate_synth_examples.node.js $opts --dsname=example2 --K=8 --noise_level=1
+./generate_synth_examples.node.js $opts --dsname=example3 --K=16 --noise_level=1
